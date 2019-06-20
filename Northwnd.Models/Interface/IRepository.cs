d39f7b2885e9e639e6b5c040e49cdbfe139fe353
yaojen <yaojenliang@gmail.com>
2019-06-20 15:17:54 +0800

    move Models Interface and Repository to New Project

---
+++ Northwnd.Models/Interface/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwnd.Models.Interface
+{
+    public interface IRepository<TEntity> : IDisposable 
+        where TEntity:class
+    {
+        void Create(TEntity instance);
+
+        void Update(TEntity instance);
+
+        void Delete(TEntity instance);
+
+        TEntity Get(Expression<Func<TEntity, bool>> predicate);
+
+        IQueryable<TEntity> GetAll();
+
+        void SaveChanges();
+    }
+}
